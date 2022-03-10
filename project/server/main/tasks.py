a0c6c33cadfa9a2beb292a5d7c182ca99b1dc065
edubergeek <dodds@hawaii.edu>
2022-03-09 18:18:09 -1000

    Add logging and database support
    Add logging support
    Add database API singleton object
    Generate download URL for image file
    Remove BundleID lookup
    Strip angle brackets in input parameter obj_id
    Add SELECT query for images

--- project/server/main/tasks.py
+++ project/server/main/tasks.py
@@ -1,12 +1,15 @@
 # project/server/main/tasks.py
 
 import os
+import logging
 from rq import get_current_job
 import json
 import csv
 from COMAJSONServer import COMAAPI
+from COMADatabase import COMADB
 
 BUNDLE_PATH='/bundles'
+logging.basicConfig(filename='/usr/src/app/logs/coma.log', filemode='w', encoding='utf-8', level=logging.DEBUG)
 
 def has_digits(inStr):
   return any(char.isdigit() for char in inStr)
@@ -87,16 +90,52 @@ class Bundle:
 
 TheBundle = Bundle()
 
+#def coma_object_images(obj_id):
+#  job = get_current_job()
+#  obj_id = TheBundle.IDFromName(obj_id)
+#  directory = '%s/%s' % (BUNDLE_PATH,obj_id)
+# 
+#  image_files = []
+#  # iterate over files in that directory
+#  for root, dirs, files in os.walk(directory):
+#    for filename in files:
+#      image_files.append("/COMA" + os.path.join(root, filename))
+#
+#  response = json.dumps(image_files)
+#  return response
+
+TheCOMADB = COMADB()
+
+def fits_download_url(fitsfile):
+  base_url = "https://coma.ifa.hawaii.edu:/coma/"
+  base_path = "/www/"
+  path_part = fitsfile.split('/')
+  base_dir = '/'.join(path_part[3:-1])
+  base_path = '/'.join(path_part[3:])
+  url = base_url + base_path + '.fits'
+  cmd = "mkdir -p /www/%s; ln -s %s /www/%s.fits" %(base_dir, fitsfile, base_path)
+  logging.debug(cmd)
+  os.system(cmd)
+  return url
+
 def coma_object_images(obj_id):
   job = get_current_job()
-  obj_id = TheBundle.IDFromName(obj_id)
-  directory = '%s/%s' % (BUNDLE_PATH,obj_id)
- 
+
+  #add this back later when object ids are normalized
+  #obj_id = TheBundle.IDFromName(obj_id)
+  obj_id = obj_id.replace("<","").replace(">","")
+
+  query = "select CONCAT(CONCAT(filepath, '/'), filename) as imagefile from coma.images where imagetype = 'OBJECT' and object = '%s';" % (obj_id)
+  TheCOMADB.Run(query)
+
   image_files = []
-  # iterate over files in that directory
-  for root, dirs, files in os.walk(directory):
-    for filename in files:
-      image_files.append("/COMA" + os.path.join(root, filename))
+  # serialize results into JSON
+  row_headers=TheCOMADB.GetResultHeaders()
+  row_values = TheCOMADB.GetResults()
+  for row in row_values:
+    url = fits_download_url(row[0])
+    image_files.append(url)
+    #image_files.append(dict(zip(row_headers,row)))
 
   response = json.dumps(image_files)
   return response
@@ -106,14 +145,14 @@ TheCOMAAPI = COMAAPI()
 def coma_fits_header(fits):
   job = get_current_job()
   TheCOMAAPI.SetUUID(job.id)
-  return TheCOMAAPI.FITSDescribe(fits)
+  return TheCOMAAPI.DescribeFITS(fits)
 
 def coma_fits_photometry(fits, objid, method, aperture):
   job = get_current_job()
   TheCOMAAPI.SetUUID(job.id)
   return TheCOMAAPI.CalibrateFITSPhotometry(fits, objid, method, aperture)
 
-def coma_fits_calibrate(fits, objid, method, aperture):
+def coma_fits_calibrate(fits):
   job = get_current_job()
   TheCOMAAPI.SetUUID(job.id)
   return TheCOMAAPI.CalibrateFITS(fits)
